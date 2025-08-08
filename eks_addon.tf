918932d23eed29438a88251e45ab9eb3c2d233d9
eswar-sudo <eswarkumar501@gmail.com>
2025-08-08 13:05:29 +0530

    Update eks_addon.tf

--- eks_addon.tf
+++ eks_addon.tf
@@ -26,11 +26,4 @@ resource "aws_eks_addon" "kube_proxy" {
   resolve_conflicts_on_update   = "OVERWRITE"
 }
 
-resource "aws_eks_addon" "core_dns" {
-  cluster_name      = var.cluster_name
-  addon_name        = "coredns"
-  addon_version     = var.coredns_version
-  resolve_conflicts_on_create   = "OVERWRITE"
-  resolve_conflicts_on_update   = "OVERWRITE"
-  service_account_role_arn = aws_iam_role.eks_admin.arn
-}
+
